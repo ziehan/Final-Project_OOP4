34e338fef300fb600e451f357182cb73c04b9548
rifqifadill <rifqiitsnain7@gmail.com>
2025-12-02 21:22:39 +0700

    PlayScreen
    Masih Error

--- frontend/core/src/main/java/com/isthereanyone/frontend/tasks/BaseTask.java
+++ frontend/core/src/main/java/com/isthereanyone/frontend/tasks/BaseTask.java
@@ -1,4 +1,12 @@
 package com.isthereanyone.frontend.tasks;
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.isthereanyone.frontend.entities.Player;
+
 public class BaseTask {
+    public void interact(Player player) {
+    }
+
+    public void render(ShapeRenderer shapeRenderer) {
+    }
 }
--- frontend/core/src/main/java/com/isthereanyone/frontend/tasks/TaskFactory.java
+++ frontend/core/src/main/java/com/isthereanyone/frontend/tasks/TaskFactory.java
@@ -1,4 +1,7 @@
 package com.isthereanyone.frontend.tasks;
 
 public class TaskFactory {
+    public static BaseTask createTask(String simple, int i, int i1) {
+    return null;
+    }
 }
